ef9de8227d52e641a2bdaceb3f0295dc8813278a
chunk <chunk9@163.com>
2024-10-13 15:22:01 +0800

    stage

--- zsim-ubuntu1804-docker/build.sh
+++ zsim-ubuntu1804-docker/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PROJECT="chunk-legion:5000/zsim-ubuntu1804"
 tag='latest'
-sudo resolvectl flush-caches
+resolvectl flush-caches
 if [ "$1" = "-c" ]
 then
 	docker builder prune
