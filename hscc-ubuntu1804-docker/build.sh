5721ccf6aed1ef46c9ab9c32647e4df83375e3e6
chunk <chunk9@163.com>
2024-10-20 10:37:15 +0800

    stage

---
+++ hscc-ubuntu1804-docker/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+PROJECT="chunk-legion:5000/zsim-ubuntu1804"
+tag='latest'
+resolvectl flush-caches
+if [ "$1" = "-c" ]
+then
+	docker builder prune
+	docker build --no-cache -t $PROJECT:$tag .
+else
+	docker build -t $PROJECT:$tag .
+fi
